b9f0c9795002c3a3a2140dc4ec335b2b81a8c0a0
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2020-08-18 00:15:51 +0100

    Delete 01.01. Print Characters.js

--- 8_Text Processing/Text Processing - Exercise/Text Processing - Lab/01.01. Print Characters.js
+++
@@ -1,9 +0,0 @@
-function solve(str){
-    // for(let i = 0; i < str.length; i++){
-    //     console.log(str[i]);        
-    for(let element of str){
-        console.log(element);        
-    }
-}
-solve('AWord');
-
