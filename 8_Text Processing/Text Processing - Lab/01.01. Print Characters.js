b1b83802f5b5c5ae9156945259bd4354335bb9cc
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2020-08-18 00:17:11 +0100

    Add files via upload

---
+++ 8_Text Processing/Text Processing - Lab/01.01. Print Characters.js
@@ -0,0 +1,8 @@
+function solve(str){
+    // for(let i = 0; i < str.length; i++){
+    //     console.log(str[i]);        
+    for(let element of str){
+        console.log(element);        
+    }
+}
+solve('AWord');
---
+++ 8_Text Processing/Text Processing - Lab/01.03. Substring.js
@@ -0,0 +1,4 @@
+function solve(string, startIndex, count){
+    console.log(string.substring(startIndex, startIndex + count));
+}
+solve("ASentance", 1, 8);
