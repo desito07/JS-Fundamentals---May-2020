42fb61ef737dd2d581d8f946f4fe1a6787e52d5b
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2020-08-18 00:15:59 +0100

    Delete 01.02. Remove Occurrences.js

--- 8_Text Processing/Text Processing - Exercise/Text Processing - Lab/01.02. Remove Occurrences.js
+++
@@ -1,9 +0,0 @@
-function solve(word, text){
-    // let result = text.replace(word, '');
-
-    while(text.indexOf(word) > -1){
-        text = text.replace(word, '');        
-    }  
-    console.log(text); 
-} 
-solve('ice', 'kicegiciceeb');
